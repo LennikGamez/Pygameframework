98cb9ea9f580d99e682e26bd5688f028fcd79021
BuildTools <unconfigured@null.spigotmc.org>
2023-09-09 21:18:39 +0200

    failed merge

---
+++ framework/particle.py
@@ -0,0 +1,24 @@
+import random
+
+from framework.render import Render
+from framework.vectorclass import Vector
+
+class Particle:
+    def __init__(self):
+        self.particles = []
+        self.emitting = False
+
+    def renderParticle(self, particle):
+        """Overwriteable"""
+
+
+    def emit(self):
+        for particle in self.particles:
+            self.renderParticle(particle)
+
+    def addParticles(self, *params: list, count=1):
+        for _ in range(count):
+            self.particles.append(*params)
+
+    def deleteParticle(self, particle):
+        self.particles.remove(particle)
---
+++ framework/screen.py
@@ -0,0 +1,32 @@
+import pygame
+from framework.helperfunctions import tuple2vec
+
+
+class Screen:
+    DISPLAY = None
+    layers = []
+
+    @staticmethod
+    def initScreen(width=500, height=500, layers=1):
+        Screen.setupScreen(width, height)
+        Screen.layers = [pygame.Surface((width, height), flags=pygame.SRCALPHA) for _ in range(layers)]
+
+    @staticmethod
+    def setupScreen(width, height):
+        Screen.DISPLAY = pygame.display.set_mode([width, height])
+    @staticmethod
+    def getLayer(index):
+        return Screen.layers[index]
+
+
+    @staticmethod
+    def getSize(index=None):
+        """returns the window dimensions"""
+        if index is None:
+            return tuple2vec(Screen.DISPLAY.get_size())
+        return Screen.DISPLAY.get_size()[index]
+
+    @staticmethod
+    def center():
+        """returns the center Vector of the window"""
+        return tuple2vec(Screen.getSize() / 2)
